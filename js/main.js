883831de676317c82b78039475956c9ce10070bb
SANJAY SINGH <rockstarss7488@gmail.com>
2024-02-06 23:54:14 +0530

    ok

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ $(document).ready(function () {
     //   $(".nav-link dropdown-toggle").click(function(){
     // $("this").css(1000,function(){
     //     console.log("It is shown.")
-    $(".nav-link dropdown-toggle").mouseleave(function () {
-        $(".nav-link dropdown-togglex").css("background", "purple");
-        $(".nav-link dropdown-toggle").css("color", "orange");
-        var a = $(".nav-link dropdown-toggle").html();
-        console.log(a);
-    });
+    // $(".nav-link dropdown-toggle").mouseleave(function () {
+    //     $(".nav-link dropdown-togglex").css("background", "purple");
+    //     $(".nav-link dropdown-toggle").css("color", "orange");
+    //     var a = $(".nav-link dropdown-toggle").html();
+    //     console.log(a);
+    // });
+
+    $("#contactbtn").slideToggle(function(){
+        $(this).css("background-color", "yellow");
+        }, function(){
+        $(this).css("background-color", "pink");
+      });
+      
 });
