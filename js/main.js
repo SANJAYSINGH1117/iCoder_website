ffb9d4f11de71e1f9211ae3bb8fa6a738c2b5472
SANJAY SINGH <rockstarss7488@gmail.com>
2024-02-08 01:38:31 +0530

    h3 id updated

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ $(document).ready(function () {
     $(this).css("background-color", "green");
   });
 
+
+
+
+  $("#h3").hover(function(){
+    $(this).css("background-color", "blue");
+    }, function(){
+    $(this).css("background-color", "white");
+  });
+
       
   // $("#contactbtn").slideToggle();
 
